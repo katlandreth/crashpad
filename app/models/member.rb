f7a73589bd13932876892e6b9ab50addbda4990f
Kat Landreth <katlandreth@gmail.com>
2016-06-15 11:53:59 -0500

    fixes missing validation error messages

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,11 +5,11 @@ class Member < ActiveRecord::Base
   has_one :host_role
   has_one :profile
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
 
-  validates :email, uniqueness: { case_sensitive: true }
-  validates_format_of :email, with: /.+@.+\..+/i
-
+  validates :email, uniqueness: { case_sensitive: true, message: "%{value} needs to be unique" }
+  validates_format_of :email, with: /.+@.+\..+/i, message: "%{value} needs to be in the format email@example.com"
   validates :password, presence: true, length: { minimum: 8 }
+  validates :password, confirmation: true
 
 end
