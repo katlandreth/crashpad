43c02b61c1c82f41fd9337fcdb22335bbddae399
Kat Landreth <katlandreth@gmail.com>
2016-06-24 12:13:18 -0500

    adds gems and config files

--- app/assets/javascripts/upload_preview.js
+++ app/assets/javascripts/upload_preview.js
@@ -1,14 +1,10 @@
 var ready;
 ready = function() {
-  console.log('document ready');
   $("body").on('change', '.file', function(event){
-    console.log('input change');
     var preview = $(".upload-preview img");
-    console.log('assigned preview variable');
     var input = $(event.currentTarget);
     var file = input[0].files[0];
     var reader = new FileReader();
-    reader.onload = function(e){
     preview.show();
     image_base64 = e.target.result;
     preview.attr("src", image_base64);
@@ -21,7 +17,6 @@ ready = function() {
     var preview = $(".upload-preview img");
     preview.hide();
   });
-
 };
 
 $(document).ready(ready);
--- app/uploaders/profile_image_uploader.rb
+++ app/uploaders/profile_image_uploader.rb
@@ -4,11 +4,23 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+ include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
+
+  if Rails.env.test?
+    storage = :file
+  end
+
+  # Use AWS storage if in production
+  if Rails.env.production? || Rails.env.development?
+    storage = :fog
+  end
+
+
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -17,12 +29,13 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  'mystery-man.png' #rails will look at 'app/assets/images/mystery-man.png'
+   end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -31,10 +44,10 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  #Create different versions of your uploaded files:
+  version :thumb do
+    process :resize_to_fill => [200, 200]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
