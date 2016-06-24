6c795b82d50d7be817368e055f130f214e8c29d5
Kat Landreth <katlandreth@gmail.com>
2016-06-24 12:39:23 -0500

    use carrierwave default instead of custom image default helper

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,14 +1,14 @@
 module ProfileHelper
 
-  def display_profile_image(member)
-    if member && member.profile && member.profile.image.length > 0
-      capture do
-        concat image_tag(member.profile.image, size: "200x200")
-        concat link_to "delete", profile_remove_image_path(member.profile.id), data: { confirm: "You sure?" }
-      end
-    else
-      image_tag("mystery-man.png", size: "200x200")
-    end
-  end
+  # def display_profile_image(member)
+  #   if member && member.profile && member.profile.image && member.profile.image.length > 0
+  #     capture do
+  #       concat image_tag(member.profile.image, size: "200x200")
+  #       concat link_to "delete", profile_remove_image_path(member.profile.id), data: { confirm: "You sure?" }
+  #     end
+  #   else
+  #     image_tag("mystery-man.png", size: "200x200")
+  #   end
+  # end
 
 end
