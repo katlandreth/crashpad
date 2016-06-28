9dfa74a9b3e1b33fd3afd7071a7d47abe2ab6cde
Kat Landreth <katlandreth@gmail.com>
2016-06-28 13:03:34 -0500

    adds error message if user abandons siguup before membership creation

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,6 +1,6 @@
-// $(document).on('click', 'button.not-signed-in', function(){
-//   $('#js-flash-message').addClass('flash_open flash_error').html("Your Crashpad account was not created.");
-// });
+$(document).on('click', 'button.not-signed-in', function(){
+  $('#js-flash-message').addClass('flash_open flash_error').html("Your Crashpad account was not created.");
+});
 //
 // $(document).on('click', 'button.signed-in', function(){
 //   $('#js-flash-message').addClass('flash_open flash_success').html("Your Crashpad account was created.");
