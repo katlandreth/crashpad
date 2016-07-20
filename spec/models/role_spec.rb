721f215be147705cd21ecfdc0914fb4c2d39e8f7
Kat Landreth <katlandreth@gmail.com>
2016-07-20 09:54:48 -0500

    write failing unit test for Role

---
+++ spec/models/role_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+member = FactoryGirl.build(:member)
+host = FactoryGirl.build(:host_role)
+guest = FactoryGirl.build(:guest_role)
+
+
+RSpec.describe Role do
+  it 'returns host if the member has only a host role ' do
+    role = Role.new(host, member)
+    role_kind = role.kind
+    expect(role_kind).to eq 'host'
+  end
+end
