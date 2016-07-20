5da32be2362ca053147fad9588ee0a312787f70b
Kat Landreth <katlandreth@gmail.com>
2016-07-20 10:48:38 -0500

    makes test pass by adding attr_accessor to dashboard model

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,6 +1,9 @@
 class Dashboard
+  attr_accessor :member, :role, :profile
 
-  def initialize(member, profile, role)
+  def initialize(member, role, profile)
+    @member = member
+    @role = role
     @profile = profile
   end
 
