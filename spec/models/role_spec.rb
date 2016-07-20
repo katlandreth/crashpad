31f50d9d18a9b71f11c25ec271b581437b057557
Kat Landreth <katlandreth@gmail.com>
2016-07-20 10:26:12 -0500

    write test for checking if the role is only a guest

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe Role do
     role_kind = role.kind
     expect(role_kind).to eq 'host'
   end
+
+  it 'returns guest if the member has only a guest role ' do
+    role = Role.new(nil, guest)
+    role_kind = role.kind
+    expect(role_kind).to eq 'guest'
+  end
 end
