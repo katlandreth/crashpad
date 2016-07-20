02218ae6968bb7a0a292217bffa4931b5fe142e6
Kat Landreth <katlandreth@gmail.com>
2016-07-20 10:46:39 -0500

    write failing test for dashboard object

---
+++ spec/models/dashboard_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+host = FactoryGirl.build(:host_role)
+guest = FactoryGirl.build(:guest_role)
+role = Role.new(host, guest)
+
+member = FactoryGirl.build(:member)
+profile = FactoryGirl.build(:profile)
+
+
+RSpec.describe Role do
+
+  it 'returns the Member object' do
+    dashboard = Dashboard.new(member, role, profile)
+    object = dashboard.member
+    expect(object).to eq member
+  end
+
+  # it 'returns the Role object' do
+  #   dashboard = Dashboard.new(member, role, profile)
+  #   object = dashboard.role
+  #   expect(object).to eq role
+  # end
+  #
+  # it 'returns the Profile object' do
+  #   dashboard = Dashboard.new(member, role, profile)
+  #   object = dashboard.profile
+  #   expect(object).to eq profile
+  # end
+
+end
