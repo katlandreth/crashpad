21b240538fd0daf4371924eac7ee1c8f7181e6cb
Kat Landreth <katlandreth@gmail.com>
2016-08-16 15:39:09 -0500

    remove commented out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # get 'members/index'
   resources :guest_roles
   resources :host_roles
   resources :dashboard
@@ -10,7 +9,6 @@ Rails.application.routes.draw do
   devise_for :members, controllers: {registrations: 'registrations'}
   resources :members, only: [:index, :show]
 
-  # get 'profiles/:id' => 'profiles#remove_image', as: :remove_image
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
