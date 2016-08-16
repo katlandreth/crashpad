944006ec9bf4d739d2478e5591c00e68d8c30e2a
Kat Landreth <katlandreth@gmail.com>
2016-08-16 15:40:43 -0500

    delete unused profile helpers

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -8,55 +8,6 @@ module MemberHelper
     end
   end
 
-  def profile_options(member)
-    if member.has_profile?
-      @profile = ProfileDecorator.new(member.profile)
-      render partial: 'shared/profile_card', locals: { profile: @profile }
-    else
-      render partial: 'profiles/new', locals: { member: member }
-    end
-  end
-
-  def member_options(member)
-    if member.has_profile?
-      @profile = ProfileDecorator.new(member.profile)
-      render partial: 'shared/member_card', locals: { profile: @profile }
-    else
-      render partial: 'profiles/new', locals: { member: member }
-    end
-  end
-
-  def role_options(member)
-    if member.has_profile?
-      @profile = ProfileDecorator.new(member.profile)
-      render partial: 'shared/role_card', locals: { profile: @profile }
-    else
-      render partial: 'profiles/new', locals: { member: member }
-    end
-  end
-
-  # def role_options(member)
-  #   if member.has_both_roles?
-  #     capture do
-  #       concat render partial: 'profiles/show', locals: { member: member }
-  #       concat render partial: 'host_roles/show', locals: { member: member }
-  #     end
-  #   elsif member.has_host_role?
-  #     render partial: 'host_roles/show', locals: { member: member }
-  #   elsif member.has_guest_role?
-  #     render partial: 'guest_roles/show', locals: { member: member }
-  #
-  #   end
-  # end
-  #
-  # def member_options(member)
-  #   if member.has_profile?
-  #     render partial: 'members/edit', locals: { member: member }
-  #   else
-  #     render partial: 'members/new', locals: { member: member }
-  #   end
-  # end
-  #
   def host_or_guest(member)
     unless member.host_role.nil?
       return "Guest"
