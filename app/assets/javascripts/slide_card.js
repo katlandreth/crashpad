f5858a640e1b40043dadbb4e0cd40d79d5228cec
Kat Landreth <katlandreth@gmail.com>
2016-08-29 10:04:28 -0500

    fixes broken edit button in dashboard by firing javascript on turbolinks load

--- app/assets/javascripts/slide_card.js
+++ app/assets/javascripts/slide_card.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on("turbolinks:load", function(){
   $('body').on('click', '.edit-card-button', function(){
     $(this).closest('.card').find('.edit-card').toggleClass('slide-in');
     $(this).toggleClass('round-button-clicked');
